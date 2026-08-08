18209a77f40435327e9206559d19e9c62f0dcbf5
Md. Maruf Ahmed Anik <bd.ahmed.anik@gmail.com>
2026-08-08 19:22:43 +0600

    Connect Web Portal and Chrome Extension to live Render cloud API https://phishguard-rl19.onrender.com

--- Extension/src/utils/constants.js
+++ Extension/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = "http://127.0.0.1:8000";
+export const API_BASE = "https://phishguard-rl19.onrender.com";
 export const RED_THRESHOLD = 0.65;
 export const YELLOW_THRESHOLD = 0.40;
 
